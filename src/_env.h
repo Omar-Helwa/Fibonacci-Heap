78566c2725cd61c3c50f598351b4ffe21c43737c
BimaTh <61094545+BimaTh@users.noreply.github.com>
2024-12-26 00:36:52 +0200

    Changed the Verbose Flag to be also to false, to be changed manually while debugging

--- src/_env.h
+++ src/_env.h
@@ -1,7 +1,9 @@
 #ifndef _ENV_H
 #define _ENV_H
 
-bool static FLAG = true; ///< Flag Button to enable/disable development and verbose features
+bool static FLAG_VERBOSE = false;///< Flag Button to enable/disable development and verbose features
+
+bool static FLAG_DEV = false;///< Flag Button to enable/disable development and verbose features
 
 const float nodeRadius = 25.0f; ///< Radius of the nodes in the visualization.
 static const int MAX_NODES = 10000; ///< Maximum number of nodes to visualize.

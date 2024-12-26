a37e41cf93dcdfb279a9c99d8f446654a4263f92
BimaTh <61094545+BimaTh@users.noreply.github.com>
2024-12-26 03:15:00 +0200

    Changed the Hospital Task Manager

--- include/patient.h
+++ include/patient.h
@@ -5,6 +5,7 @@
 #include <string>
 
 // forward dec
+class VisualizeTaskManager;
 
 /**
  * @class Patient
@@ -17,22 +18,38 @@ private:
     int age; ///< The age of the patient.
 
 public:
-
+    friend class VisualizeTaskManager;
     /**
      * @brief Constructor to initialize a Patient object with all details.
      * @param description The name of the patient.
      * @param age The age of the patient.
      * @param gender The gender of the patient.
      */
-
-    Patient(){};
+    Patient(std::string description, int age, std::string gender) :
+    description(description), age(age),gender(gender) {}
 
     /**
      * @brief Destructor for the Patient object.
      */
     ~Patient() = default;
 
+    std::string getDescription() const;
 
+    std::string getGender() const;
+
+    int getAge() const;
 };
+
+std::string Patient::getDescription() const {
+    return description;
+}
+
+std::string Patient::getGender() const {
+    return gender;
+}
+
+int Patient::getAge() const {
+    return age;
+}
 
 #endif // PATIENT_H
--- src/VisualizeTaskManager.h
+++ src/VisualizeTaskManager.h
@@ -4,10 +4,11 @@
 #include "imgui.h"
 #include "HospitalTaskManager.h"
 #include "error_handler.h"
+#include "patient.h"
 
 class VisualizeTaskManager {
 public:
-    void visualize(HospitalTaskManager &manager, error_handler * error_handler) {
+    void visualize(HospitalTaskManager &manager, error_handler *error_handler) {
         // Set global font scale
 
         // Set a larger window size
@@ -17,26 +18,46 @@ public:
         ImVec2 contentRegion = ImGui::GetContentRegionAvail();
 
         // Input controls for adding a task
-        static int taskPriority = 0;
-        static char taskDescription[128] = "";
+        // static int taskPriority = 0;
+        // static char taskDescription[128] = "";
 
-        ImGui::InputText("Description", taskDescription, IM_ARRAYSIZE(taskDescription));
-        ImGui::InputInt("Priority", &taskPriority);
+        // ImGui::InputText("Description", taskDescription, IM_ARRAYSIZE(taskDescription));
+        // ImGui::InputInt("Priority", &taskPriority);
+        if (ImGui::BeginPopup("Add Patient")) {
+            // Input controls for adding a task
+            static int taskPriority = 0;
+            static char Name[128] = "";
+            static int Age = 0;
+            static char Description[128] = "";
+            static int genderIndex = 0;
+            const char *genderItems[] = {"Male", "Female"};
 
+            ImGui::InputText("Name", Name, IM_ARRAYSIZE(Name));
+            ImGui::InputInt("Age", &Age);
+            ImGui::Combo("Gender", &genderIndex, genderItems, IM_ARRAYSIZE(genderItems));
+            ImGui::InputText("Description", Description, IM_ARRAYSIZE(Description));
+            ImGui::InputInt("Priority", &taskPriority);
+
+            if (ImGui::Button("Insert")) {
+                Patient *newPatient = new Patient(Description, Age, genderItems[genderIndex]);
+                manager.addTask(Name, taskPriority);
+
+                ImGui::CloseCurrentPopup();
+            }
+
+            ImGui::EndPopup();
+        }
         // Button to add a task
-        if (ImGui::Button("Add Task")) {
-            // 40% width, 70px height
-            std::string descriptionStr(taskDescription);
-            manager.addTask(descriptionStr, taskPriority);
+        if (ImGui::Button("Insert Patient")) {
+            ImGui::OpenPopup("Add Patient");
         }
 
         ImGui::SameLine();
+
         // Button to complete the highest priority task
         if (ImGui::Button("Complete Highest Priority Task")) {
-            // 50% width, 70px height
             manager.completeHighestPriorityTask();
         }
-        // ImGui::PopStyleVar();
 
         ImGui::Separator();
 
@@ -54,6 +75,32 @@ public:
 
         ImGui::Separator();
 
+        static int displayPriority = 0;
+        ImGui::InputInt("Priority", &displayPriority);
+
+        ImGui::SameLine();
+
+        if (ImGui::BeginPopup("Display Patient")) {
+            Node<std::string> *temp = manager.getTaskCopy(displayPriority);
+
+            if (temp != nullptr && temp->getData() != nullptr) {
+                ImGui::Text("Name: %s", temp->getName().c_str());
+                ImGui::Text("Priority: %d", temp->getKey());
+                ImGui::Text("Description: %s", temp->getData()->getDescription().c_str());
+                ImGui::Text("Age: %d", temp->getData()->getAge());
+                ImGui::Text("Gender: %s", temp->getData()->getGender().c_str());
+            } else {
+                ImGui::Text("No patient found with the given priority.");
+            }
+            ImGui::EndPopup();
+        }
+
+        if (ImGui::Button("Display Data")) {
+            ImGui::OpenPopup("Display Patient");
+        }
+
+        ImGui::Separator();
+
         error_handler->renderErrorMessages();
 
         ImGui::Separator();
@@ -61,7 +108,7 @@ public:
         // Display the number of tasks
         ImGui::Text("Number of Tasks: %d", manager.countTasks());
 
-            displayTasks(manager);
+        displayTasks(manager);
 
 
         ImGui::End();
